de1621802a0bad72684dbe8aa1a1517cb0597df2
Dasmrinal934 <dasmrinal934@gmail.com>
2019-03-14 21:43:28 +0530

    first commit

---
+++ header.php
@@ -0,0 +1,66 @@
+<!DOCOTYPE html>
+	<html lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Mrinal DAs">
+  	<meta name="web_author" content="">
+  	<meta name="reply-to" content="dasmrinal934@gmail.com">
+  	<meta name="contact" content="dasmrinal934@gmail.com">
+		
+			<title>HILLOFF SOFTWARE PVT LTD</title>
+			<link rel="icon" type="image/jpg" href="img/logo.jpg" />
+			<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <!-- Custom Style -->
+    <link href="css/mrinal.css" rel="stylesheet">
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/font-awesome.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+
+
+
+</head>
+<body>
+		<body>
+
+ <nav class="navbar navbar-inverse">
+
+					<div class="container-fluid">
+						<div class="navbar-header">
+							 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#collapsibleNavbar">
+   
+	 <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      
+  </button>
+    
+ <a class="navbar-brand" href="index.php" style="color:#fff"><h3>HILL<span class="themecolor">OFF</span> COMPUTERS</h3></a>
+							</div>
+							<div class="collapse navbar-collapse" id="collapsibleNavbar">
+  									<ul class="nav navbar-nav navbar-right">
+									<li><a href="index.php"><h5> Home </h5> </a></li>
+									<li><a href="about.php"> <h5>About</h5> </a></li>
+									<li><a href="works.php"> <h5>Works and Team</h5> </a></li>
+									<li><a href="service.php"><h5> Services</h5> </a></li>
+									<li><a href="https://www.google.co.in/search?ei=Nz6JXIW3NpnYz7sP3qiS6Ak&q=free+software&oq=free+software&gs_l=psy-ab.3..0l5j0i131j0l4.4302.8426..10018...0.0..0.290.3074.2-13......0....1..gws-wiz.......0i71j35i39j0i131i67j0i67.ph06VZQWCJo"><h5>Free Sofrware</h5> </a></li>
+									<li><a href="contact.php"> <h5>Contact </h5></a></li>
+
+								</ul>
+
+							</div>
+
+						</div>
+				</nav>
+</body>
+</html>
