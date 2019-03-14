90cff3f272467ceb68ad405567bf1c5574ba4b7f
Dasmrinal934 <dasmrinal934@gmail.com>
2019-03-14 21:42:33 +0530

    first commit

---
+++ contact.php
@@ -0,0 +1,73 @@
+<?php include "header.php" ?>
+<div style="background-color:#01a2a6">
+<div class="container">
+	<div class="row">
+		<div class="col-xs-12" style="color:#032c4c" style="text-shadow:#CC66FF">
+			<h1 style="text-align:center"> SEND A QUICK MESSAGE 
+			<br />
+			
+			TO REACH YOU
+			</h1>
+			</div>
+		</div>
+			<div class="col-md-6 col-sm-8 col-xs-12" style="width:1000px; margin:0 auto; padding-left:100px">
+				<div class="col-sm-4 col-xs-12">
+					<div class="form-group">
+					<select class="form-control">
+						<option> Mr</option>
+						<option> Mrs</option>
+					</select>
+					</div>
+				</div>
+				<div class="col-sm-4 col-xs-12">
+					<div class="form-group"> <input type="text" class="form-control" id="FirstName" placeholder="First Name" />
+					</div>
+					</div>
+					<div class="col-sm-4 col-xs-12">
+					<div class="form-group"> <input type="text" class="form-control" id="LastName" placeholder="Last Name" />
+					</div>
+					</div>
+					<div class="col-sm-4 col-xs-12">
+					<div class="form-group"> <input type="tel" class="form-control" id="ContactNumber" placeholder="Contact Number" />
+					</div>
+					</div>
+					<div class="col-sm-4 col-xs-12">
+					<div class="form-group"> <input type="email" class="form-control" id="ImputEmail" placeholder="Email" />
+					</div>
+					</div>
+					<div class="col-xs-12">
+						<div class="form-group">
+						<textarea class="form-control" rows="5" placeholder="Your Message"> </textarea>
+						</div>
+						<hr />
+						<button type="submit" > Submit </button>
+						</div>
+						</div>
+						</div>
+					
+				
+			</div>
+			
+			<div class="row">
+				<div class="col-md-6"  ss="col-md-6 col-sm-8 col-xs-12" style="width:500px; margin:0 auto; padding-left:100px">
+			<h2>	<i class="fa fa-map-marker"</i></i></h2>
+					<h2> Address</h2>
+					<p> Haropur Doltala, Pratapnagar, Sonarpur</p>
+					<p> South 24 Parganas, West Bengal</p>
+					<P> <br /></P>
+					<h3> Call</h3>
+					<p> +91 8240676075</p>
+					<br />
+					<h3> Email - </h3>
+					<p> dasmrinal934@gmail.com</p>
+					<p> dmrinal824@gmail.com</p>
+				</div>
+					<div class="col-md-6"  ss="col-md-6 col-sm-8 col-xs-12" style="width:500px; margin:0 auto; padding-left:100px">
+					<img src="img/cm.jpg" class="img-responsive" alt="working">
+					</div>
+				
+			</div>
+	</div>
+	
+	
+	<?php include "footer.php" ?>
