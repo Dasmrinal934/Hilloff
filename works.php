a5a09fae88a0c63f6f1aa92b145c860c322357d9
Dasmrinal934 <dasmrinal934@gmail.com>
2019-03-14 21:44:11 +0530

    first commit

---
+++ works.php
@@ -0,0 +1,183 @@
+<?php include "header.php" ?>
+<div style="background-image:url(img/someworks1.jpg)">
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<h3 class="title" style="text-align:center">
+			RECENT
+				<span class="themecolor"> WORKS</span>
+			</h3>
+			<p class="a-slog" style="text-align:center"> We are Design any type of Web Site
+			</p>
+		</div>
+	</div>
+	<div class="gap"></div>
+		<div class="row">
+			<div class="col-sm-4 port-item margin-bottom">
+				<div class="item-img-wrap">
+					<img src="img/hard1.jpg" class="img-responsive" alt="working">
+						<div class="item-img-overlay">
+							<a href="www.jdb-itcare.com" class="btn btn-default">
+							GO TO SITE<span>
+							</span>  	
+							</a>
+						</div>
+				</div>
+				<div class="work-desc">
+					<h3 style="background-color:#FF00FF; text-align:center"> <a href="www.jdb-itcare.com"> JDB ITCARE COMPUTERS PVT LTD</a>
+					</h3>
+					
+				
+				</div>
+			
+			</div>
+			<div class="col-sm-4 port-item margin-bottom">
+				<div class="item-img-wrap">
+					<img src="img/ecommerce1.jpg" class="img-responsive" alt="working">
+						<div class="item-img-overlay">
+							<a href="https://dasarchana185.000webhostapp.com/" class="btn btn-default">
+							GO TO SITE <span>
+							</span>  	
+							</a>
+						</div>
+				</div>
+				<div class="work-desc">
+					<h3 style="background-color:#FF00FF; text-align:center"> <a href="https://dasarchana185.000webhostapp.com/"> HILLOFF </a>
+					</h3>
+					
+				
+				</div>
+			
+			</div>
+			
+			<div class="col-sm-4 port-item margin-bottom">
+				<div class="item-img-wrap">
+					<img src="img/soft1.jpg" class="img-responsive" alt="working">
+						<div class="item-img-overlay">
+							<a href="http://deb-host.000webhostapp.com/" class="btn btn-default">
+							GO TO SITE <span>
+							</span>  	
+							</a>
+						</div>
+				</div>
+				<div class="work-desc">
+					<h3 style="background-color:#FF00FF; text-align:center"> <a href="http://deb-host.000webhostapp.com/"> JDB ITCARE COMPUTERS </a>
+					</h3>
+					
+				
+				</div>
+			
+			</div>
+			</div>
+</div>
+</div>
+
+<div style="background-image:url(img/team2.jpg)">
+
+
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<h3 class="title" style="text-align:center">
+			OUR
+				<span class="themecolor"> TEAM</span>
+			</h3>
+			<p class="a-slog" style="text-align:center"> We are Design any type of Web Site
+			</p>
+		</div>
+	</div>
+	
+		<div class="row text-center">
+		<div class="col-sm-3">
+			<img src="img/anupamda.png"  height="300" width="374" alt="team-member" class="img-responsive">
+			<h4 style="color:#FF0000"> Anupam Mondal</h4>
+			<h5> Founder </h5>
+			<p>Hey, I am ANupam Mondal residing in this beautiful world. I create websites and mobile.</p>
+			<ul class="list-inline top20">
+				<li>
+					<a href="#">
+						<i class="fa fa-facebook"></i>
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<i class="fa fa-twitter"></i>
+					</a>
+				</li>
+				
+
+			</ul>
+			
+		</div>
+			<div class="col-sm-3">
+			<img src="img/design.jpg" height="247" width="200" alt="team-member" class="img-responsive">
+			<h4> Deb Das</h4>
+			<h5> Designer </h5>
+			<p>Hey, I am Deb Das residing in this beautiful world. I create websites and mobile.</p>
+			<ul class="list-inline top20">
+				<li>
+					<a href="#">
+						<i class="fa fa-facebook"></i>
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<i class="fa fa-twitter"></i>
+					</a>
+				</li>
+				
+
+			</ul>
+			
+		</div>
+				<div class="col-sm-3">
+			<img src="img/mrinal.jpg"  height="350" width="600"alt="Full Strack Developer" class="img-responsive">
+			<h4 style="color:#FF0000"> Mrinal Das</h4>
+			<h5> Full Strack Developer</h5>
+			<p>Hey, I am  Mrinal Das residing in this beautiful world. I create websites and mobile.</p>
+			<ul class="list-inline top20">
+				<li>
+					<a href="#">
+						<i class="fa fa-facebook"></i>
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<i class="fa fa-twitter"></i>
+					</a>
+				</li>
+				
+
+			</ul>
+			
+		</div>
+		 					<div class="col-sm-3">
+			<img src="img/web.jpg"  height="350" width="600"alt="Full Strack Developer" class="img-responsive">
+			<h4> Mrinal Das</h4>
+			<h5> Web Developer</h5>
+			<p>Hey, I am  Mrinal Das residing in this beautiful world. I create websites and mobile.</p>
+			<ul class="list-inline top20">
+				<li>
+					<a href="#">
+						<i class="fa fa-facebook"></i>
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<i class="fa fa-twitter"></i>
+					</a>
+				</li>
+				
+
+			</ul>
+			
+		</div>
+		 
+		
+		</div>
+		</div>
+		</div>
+		
+<?php include "footer.php" ?>
