a4295523910be03af174ad270e2eefb960f48489
Dasmrinal934 <dasmrinal934@gmail.com>
2019-03-14 21:44:03 +0530

    first commit

---
+++ service.php
@@ -0,0 +1,86 @@
+<?php include "header.php" ?>
+<head>
+<link href="css/mrinal.css" rel="stylesheet">
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/font-awesome.min.css" rel="stylesheet">
+</head>
+<div style="background-image:url(img/services.jpg)">
+  <div class="container">
+  	<div class="col-md-12">
+		<h3 class="title" style="text-align:center"> We Are <span class="themecolor">Good In </span></h3>
+	</div>
+	<div class="gap"> </div>
+		<div class="row">
+			<div class="col-md-4">
+			<div class="m-service margin-bottom">
+<i class="fa fa-diamond" aria-hidden="true"></i>
+<div class="m-service-detail">
+			<h4 style="color:#FFFFFF">Strategy Solutions</h4>
+			<p style="color:#CCCCCC"> Accessing internet only on desktops is a thing of yesteryears. The customer, these days, have access to plethora of interfaces like tablets, notebooks, mobiles, etc. to get connected to the internet.</p>
+			</div>
+			
+			</div>
+			</div>
+			<div class="col-md-4">
+			<div class="m-service margin-bottom">
+<i class="fa fa-tablet" aria-hidden="true"></i>
+			<h4 style="color:#FFFFFF" style="margin-center:90px" > Strategy Solutions</h4>
+			<p style="color:#FFFFFF"> Lorem ipsum dolor sit amet ne onsectetuer adipiscing elit. Aenean commodo ligula eget dolor in tashin ty dolor site amet ne onseceruter adipiscing.</p>
+			
+			</div>
+			</div>
+			<div class="col-md-4">
+			<div class="m-service margin-bottom">
+<i class="fa fa-magic" style="color:#FF0000" aria-hidden="true"></i>
+			<h4 style="color:#f8198d" style="margin-center:90px" > SEO</h4>
+			<p style="color:#f8198d"> Google gets more than 100 billion searches a month, and 75% of searchers never scroll past the first page of those search results. Our Atlanta SEO experts can not only craft the right titles and tags but also create shrewd, keyword-oriented website content to help increase your organic search results.
+</p>
+			
+			</div>
+			</div>
+		</div>
+  
+  
+  <div class="row">
+			<div class="col-md-4">
+			<div class="m-service margin-bottom">
+<i class="fa fa-rocket" aria-hidden="true"></i>
+<div class="m-service-detail">
+			<h4 style="color:#FFFFFF" > Graphic Design</h4>
+			<p style="color:#CCCCCC">
+Elevate your brand with graphic design that is creative, responsive, fresh and tailor-made for you. From a custom logo design to a complete branding revamp, our graphic design skills will blow your mind.
+</p>
+			
+			</div>
+			</div>
+			</div>
+			
+			<div class="col-md-4">
+			<div class="m-service margin-bottom">
+<i class="fa fa-rocket" aria-hidden="true"></i>
+<div class="m-service-delicios">
+			<h4 style="color:#f8198d" > Unlimited Colors</h4>
+			<p style="color:#f8198d">
+We put a lot of effort in design, as it’s the most important ingredient of successful website.Sed ut perspiciatis unde omnis iste natus error sit.
+</p>
+			
+			</div>
+			</div>
+			</div>
+			<div class="col-md-4">
+			<div class="m-service margin-bottom">
+<i class="fa fa-pencil-square"  style="color:#0000FF"aria-hidden="true"></i>
+<div class="m-service-delicios">
+			<h4 style="color:#0000FF" > Unlimited Colors</h4>
+			<p style="color:#0000FF">
+We put a lot of effort in design, as it’s the most important ingredient of successful website.Sed ut perspiciatis unde omnis iste natus error sit.
+</p>
+			
+			</div>
+			</div>
+			</div>
+			
+			</div>
+			</div>
+<?php include "footer.php" ?>
+		
