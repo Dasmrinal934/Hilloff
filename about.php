1d7d105fbc35c52c00fe141e36041e87f5fe1991
Dasmrinal934 <dasmrinal934@gmail.com>
2019-03-14 21:40:08 +0530

    first commit

---
+++ about.php
@@ -0,0 +1,45 @@
+<?php include "header.php" ?>
+<div  style="background-color:#01a2a6">
+	<h2 style="color:#FFFFFF; font-family:'Times New Roman', Times, serif; text-align:center">
+	ABOUT <span class="themecolor"> US</span>
+	</h2>
+	</div>
+	<div style="background-color:#CC66CC">
+	<div class="col-md-12 text-center black">
+	<p class="a-slog" style="font-size:18px"> The company has its head office and works at Kolkata. The present installed base is approximately a five hundred computers / peripherals which include HILLOFF COMPUTERS integrated computer systems as well as MNC products like IBM, HP, Compaq, Acer amongst others. The companys prime focus is on providing quality and efficient products, solutions and support services to its varied clientele, through its well trained and highly experienced team of skilled and certified engineers and technicians. It is because of the stringent focus to ensure efficient quality control across all spheres of its activity. In its quest to bring the best and the latest from the world of technology.
+
+</P>
+</div>
+<div class="row about-box">
+	<div class="col-sm-4 text-center">
+		<div class="margin-bottom">
+		<i class="fa fa-comment-o"> </i>
+			<h3>FREE SUPPORT</h3>
+			<p class="black" style="font-size:16px">
+			"We are makes it easy for you to create a website and grow your business online with ecommerce and SEO solutions all in one place."</p>
+		</div>
+	</div>
+
+	<div class="col-sm-4 about-line text-center">
+		<div class="margine-bottom">
+			<i class="fa fa-mobile"> </i>
+			<h4> FULLY RESPONSIVE</h4>
+			<P class="black" style="font-size:16px">
+			t only incorporate responsive design in your new website design project, but if you have a website and need to update it with responsive design, we can modify your website's design and make it responsive.
+
+
+			</P>
+		</div>
+	</div>
+		<div class="col-sm-4 text-center">
+		<div class="margin-bottom">
+		<i class="fa fa-area-chart"> </i>
+			<h3>GREAT WEB IDEA</h3>
+			<p class="black;" style="font-size:16px">
+			We are offers complete solutions in one place for your website and online business. We understand the balance between web design and web development, so that we make a unique project strategy for you for smoother work and best result.</p>
+		</div>
+	</div>
+
+</div>
+
+<?php include "footer.php" ?>
---
+++ footer.php
@@ -0,0 +1,43 @@
+<!DOCOTYPE HTML>
+<html>
+<body>
+	<nav class="navbar navbar-inverse">
+
+											<div class="navbar-footer">
+		<div class="row">
+			<div class="col-md-4">
+				<a href="#"> Terms of Use</a>
+				<a href="#"> | Privacy Policy</a>
+			</div>
+			<div class="col-md-4" style="color:#FFFFFF">
+			<small> © 2019 HILLOFF COMPUTERS. All rights reserved.
+			</small>
+				
+			</div>
+			<div class="col-md-4">
+			<ul>
+				<li>
+			<a href="https://twitter.com/" class="fa fa-twitter" style="color:#33FF99"> 
+			Twitter |
+			
+			</a>
+				
+					<a href="www.facebook.com" class="fa fa-facebook" style="color:#0000FF"> Facebook |
+					</a>
+				
+										<a href="www.youtube.com" class="fa fa-youtube" style="color:#FF0000">Youtube
+			</a>
+</li>			
+			</ul>
+			</div>
+		</div>
+	
+	</div>
+	</nav>
+	
+	
+	
+	
+	
+
+</html>
